100d3e2f2e9e60e3795261acfeac4bf092b551c0
Voitovrg <romavoitov13@gmail.com>
2024-07-21 15:03:07 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import './style/App.css';
 
 const App = () => {
+
+  const [count, setCount]  = React.useState(0);
+
+
   return (
     <div className="App">
+      <p>{count}</p>
+      <button>Click me</button>
     </div>
   );
 }
