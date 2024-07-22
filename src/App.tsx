5ae3e55654982af7593ff1b34e59699c6a3816ef
Voitovrg <128575480+Voitovrg@users.noreply.github.com>
2024-07-22 12:15:33 +0300

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, {SetStateAction, useState} from 'react';
 import './style/App.css';
 
 const App = () => {
 
-  const [count, setCount]  = React.useState(0);
+  const [count, setCount]:SetStateAction<any>  = useState(0);
 
 
   return (
     <div className="App">
       <p>{count}</p>
-      <button>Click me</button>
+      <button onClick={e => setCount(e)}>Click me or not me</button>
     </div>
   );
 }
