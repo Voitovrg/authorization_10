203bfbc16c35e0c99fd1a6d236c711e2781268c0
Voitovrg <romavoitov13@gmail.com>
2024-07-22 13:00:26 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import React, {SetStateAction, useState} from 'react';
+import React, {useState} from 'react';
 import './style/App.css';
 
-const App = () => {
+const App: React.FC = () => {
 
-  const [count, setCount]:SetStateAction<any>  = useState(0);
+    const [count, setCount] = useState<number>(0);
 
+    const increment = (): void => {
+        setCount(count + 1);
+    }
 
-  return (
-    <div className="App">
-      <p>{count}</p>
-      <button onClick={e => setCount(e)}>Click me or not me</button>
-    </div>
-  );
+    return (
+        <div className="App">
+            <p>Count: {count}</p>
+            <button onClick={increment}>Click me or not me</button>
+        </div>
+    );
 }
 
 export default App;
