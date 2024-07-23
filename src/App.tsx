78e730d91bbb4009bd4d8a75e46176c9f7bf5d2e
Voitovrg <romavoitov13@gmail.com>
2024-07-23 14:18:09 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import React, {useState} from 'react';
+import React, {} from 'react';
 import './style/App.css';
 
 const App: React.FC = () => {
 
-    const [count, setCount] = useState<number>(0);
-
-    const increment = (): void => {
-        setCount(count + 1);
-    }
-
     return (
         <div className="App">
-            <p>Count: {count}</p>
-            <button onClick={increment}>Click me or not me</button>
+            <div className={'main-box'}>
+                <div className={'main-container'}>
+
+                </div>
+            </div>
         </div>
     );
 }
